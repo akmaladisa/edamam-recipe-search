e34d9890367edc0a2cee504cdde90cac557d6433
Akmal Adi Saputra <saputraakmal02@gmail.com>
2023-01-15 12:04:46 +0700

    setup route

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
     
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 container">
-        <a href="https://github.com/akmaladisa" class="no-underline text-3xl hover:text-green-500 ease-in duration-300">
+        <a href="https://github.com/akmaladisa/edamam-recipe-search" class="no-underline text-3xl hover:text-green-500 ease-in duration-300">
             <i class="bi bi-github mr-1"></i>
             <span class="ml-1">REPO</span>
         </a>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
+import router from './router/index.js'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router)
+app.mount('#app')
