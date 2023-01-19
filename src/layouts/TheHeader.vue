261afd004b74478a8d29020238e8a3ac603d1bcb
Akmal Adi Saputra <saputraakmal02@gmail.com>
2023-01-19 22:02:38 +0700

    binding data

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-3 dark:bg-slate-600 dark:text-gray-200 container mx-auto w-96">
+    <nav class="bg-white shadow border-gray-200 px-2 sm:px-4 py-3 dark:bg-slate-600 dark:text-gray-200 container mx-auto w-96">
         <a href="https://github.com/akmaladisa/edamam-recipe-search" class="no-underline text-3xl hover:text-green-500 ease-in duration-300">
             <i class="bi bi-github mr-1 text-2xl"></i>
             <span class="ml-1 text-2xl">REPO</span> 
